c62b6a1bf93f346877025721fb1768c0a2ecd89b
Abdulhakim <Abdulthe383@gmail.com>
2025-04-06 01:51:02 -0400

    Updated Code

--- src/Component/Greetings.jsx
+++ src/Component/Greetings.jsx
@@ -1,2 +1,13 @@
+import React from "react"; // imported React   
+
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello, from the Greeting Componets!!</h2>
+      <p>This is my frist custom component</p>
+    </div> // return JSX 
+  );
+}
+export default Greeting; // export default Greeting
 
 
